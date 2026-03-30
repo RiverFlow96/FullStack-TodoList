5df64a7add143a80502db3aa842ed0bd4175a2e4
RiverFlow96 <yordanyalmeda8@gmail.com>
2026-03-30 17:17:46 -0400

    Updating settings.py for more security

--- backend/todolistapi/settings.py
+++ backend/todolistapi/settings.py
@@ -126,5 +126,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication'
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated' # Esto fuerza que todos los endpoints requieran autenticación
+    ]
 }
