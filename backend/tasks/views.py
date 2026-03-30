ee21b2ddee185d30798018cd21883f56142f36b9
RiverFlow96 <yordanyalmeda8@gmail.com>
2026-03-30 16:47:17 -0400

    Creation of the TaskViewSet

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Task
+from .serializers import TaskSerializer
 
 # Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        # Cada usuario solo ve sus propias tarea
+        return Task.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        # Asigna el usuario autenticado al crear
+        serializer.save(user=self.request.user)
