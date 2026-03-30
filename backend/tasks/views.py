7403f1076fa09b1d9dc7f800c391a39a0ce89f40
RiverFlow96 <yordanyalmeda8@gmail.com>
2026-03-30 16:59:15 -0400

    Cretion of the condition for superusers

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -8,9 +8,12 @@ class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
     
     def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return Task.objects.all()
         # Cada usuario solo ve sus propias tarea
-        return Task.objects.filter(user=self.request.user)
-    
+        return Task.objects.filter(user=user)
+            
     def perform_create(self, serializer):
         # Asigna el usuario autenticado al crear
         serializer.save(user=self.request.user)
