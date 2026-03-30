01b6481fd9f352319530eaeecba27615f29bf1da
RiverFlow96 <yordanyalmeda8@gmail.com>
2026-03-30 17:45:51 -0400

    Updating for user endpoint

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -1,6 +1,7 @@
 # Un serializer es utilizado para exportar la informacion de los modelos como formato JSON a la API
 from rest_framework import serializers
 from .models import Task
+from django.contrib.auth.models import User
 
 class TaskSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField(read_only=True)
@@ -8,5 +9,11 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
-        read_only_fieldd = ['user', 'created_at', 'updated_at']
+        read_only_fields = ['user', 'created_at', 'updated_at']
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        read_only_fields = ['id']
 
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action 
+from rest_framework.response import Response 
 from .models import Task
-from .serializers import TaskSerializer
+from .serializers import TaskSerializer, UserSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
 class TaskViewSet(viewsets.ModelViewSet):
@@ -17,3 +20,17 @@ class TaskViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         # Asigna el usuario autenticado al crear
         serializer.save(user=self.request.user)
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return User.objects.all()
+        return User.objects.filter(id=self.request.user.id)
+    
+    @action(detail=False, methods=['get'], url_path='me', url_name='me')
+    def me(self,request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
