9253f354f28231ce7c60a634460ccc70c18ce0b8
RiverFlow96 <yordanyalmeda8@gmail.com>
2026-03-30 17:13:57 -0400

    Updateing settings.py file

--- backend/todolistapi/settings.py
+++ backend/todolistapi/settings.py
@@ -125,5 +125,6 @@ CORS_ALLOWED_ORIGINS = ['http://localhost:5137']
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
     )
 }
